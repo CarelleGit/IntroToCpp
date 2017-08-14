b91bb2dbb946b3437cc49892ae02a92025091750
s179062 <s179062@SEA-8CC916>
2017-08-14 12:46:11 -0700

    Add project files.

---
+++ 1-HelloAIE/Main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello AIE" <<std::endl;
+	std::cout << "track 1" << std::endl;
+	std::cout << "track 2" << std::endl;
+	std::cout << "track 3" << std::endl;
+	std::cout << "track 4" << std::endl;
+
+	system("pause");
+
+	std::cout << "0\n1\n2\n3\n4\n5" << std::endl;
+
+	system("pause");
+
+	std::cout << "5\n4\n3\n2\n1\n0" << std::endl;
+
+	system("pause");
+	//A
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+	//B
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+}
